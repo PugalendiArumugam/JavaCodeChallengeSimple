439badf4c744c6b5010dc8112b0a57339d4fe7c3
Pugalendi Arumugam <pugal.a@gmail.com>
2025-02-18 23:27:29 +0530

    sum nums  added

--- CodeChallengeSimple/src/TwoSum.java
+++ CodeChallengeSimple/src/TwoSum.java
@@ -1,8 +1,32 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
     //************************************************************************
-    //* print number 1 to 100, but if the number is multiple of 3 print Fizz ≈
-    //* or if number is multiple of 5 print Buzz, but if number multiple of both
-    //* 3 and 5 print FizzBuzz
+    //* is there a sum of two  numbers present in a array.  Ans yes or
+    //* return the position of array from the two numbers.
+    //* eg If target is 7 is sum of two numbers present in array {2,3,1,5,7,8}
     //************************************************************************
-    
+
+    public static void main(String[] args) {
+        int[] numbers = new int[] {2,3,5,7,4,8,9};
+        int target = 4;
+        int[] result = getTwoNumbers(numbers,target);
+        System.out.println(result[1]+" "+result[0]);
+    }
+
+    public static int[] getTwoNumbers(int[] numbers,int target){
+        Map<Integer,Integer> checkednumbers = new HashMap<>();
+
+        for (int i =0;i < numbers.length; i++){
+            int delta = target - numbers[i];
+            if (checkednumbers.containsKey(delta)){
+                return new int[] {i, checkednumbers.get(delta)};
+            }
+            checkednumbers.put(numbers[i],i);
+        }
+        return new int[]{-1,-1};
+
+    }
+
 }
